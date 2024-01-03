656ea9fed290ae7740efd15c1cb4ee227f149478
rene <rene@rightcom.com>
2024-01-03 17:28:31 +0100

    Initial commit

---
+++ littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by RightMac-Rene on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
